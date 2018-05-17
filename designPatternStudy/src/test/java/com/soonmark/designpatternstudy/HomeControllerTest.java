241a4fe771a777165aa49058f10b32c97bf8d495
Soonmark <soonmarkella@naver.com>
2018-05-17 15:50:07 +0900

    테스트 코드 파일 생성

---
+++ designPatternStudy/src/test/java/com/soonmark/designpatternstudy/HomeControllerTest.java
@@ -0,0 +1,14 @@
+package com.soonmark.designpatternstudy;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HomeControllerTest {
+
+	@Test
+	public void homeControllerTest() {
+		fail("Not yet implemented");
+	}
+
+}
